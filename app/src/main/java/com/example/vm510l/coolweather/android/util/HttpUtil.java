a8384b444c71bcdd65c7c19cde79f611f341d4f9
cslg2016 <895941515@qq.com>
2018-08-20 21:20:08 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/example/vm510l/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.vm510l.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by VM510L on 2018/8/19.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
