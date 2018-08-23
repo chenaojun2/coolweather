8882d54306f2e7f894be64025b7cad157adde7d1
cslg2016 <895941515@qq.com>
2018-08-23 22:00:39 +0800

    增加后台更新和图标

--- app/src/main/java/com/example/vm510l/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/example/vm510l/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.vm510l.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.vm510l.coolweather.android.gson.Forecast;
 import com.example.vm510l.coolweather.android.gson.Weather;
+import com.example.vm510l.coolweather.android.service.AutoUpdateService;
 import com.example.vm510l.coolweather.android.util.HttpUtil;
 import com.example.vm510l.coolweather.android.util.Utility;
 
@@ -242,5 +244,7 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
